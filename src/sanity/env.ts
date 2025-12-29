fa8c7ded380ec073866621bee2631e338220d84c
Ankur Halder <ankur.halder12345@gmail.com>
2025-12-29 16:13:14 +0530

    feat: configure Sanity API environment variables

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,9 +1,7 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-12-27'
 
-const _dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-if (!_dataset) throw new Error('Missing env: NEXT_PUBLIC_SANITY_DATASET')
+const _dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 export const dataset = _dataset
 
-const _projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-if (!_projectId) throw new Error('Missing env: NEXT_PUBLIC_SANITY_PROJECT_ID')
+const _projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'c20abca7'
 export const projectId = _projectId
