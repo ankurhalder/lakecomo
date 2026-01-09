ce5df7f221727b52204f764e6283d02e058388ab
Ankur Halder <ankur.halder12345@gmail.com>
2026-01-09 23:35:54 +0530

    Update webmail redirect

--- next.config.ts
+++ next.config.ts
@@ -13,8 +13,14 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'webmail.lakecomostyle.it' }],
+        destination: 'https://server123.web-hosting.com:2096/:path*',
+        permanent: true,
+      },
+      {
         source: '/webmail',
-        destination: 'https://webmail.lakecomostyle.it',
+        destination: 'https://server123.web-hosting.com:2096',
         permanent: true,
       },
       {
